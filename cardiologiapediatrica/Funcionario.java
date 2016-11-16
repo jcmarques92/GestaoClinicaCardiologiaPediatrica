9e1476fdce959d9434a13319161ed905f27c302b
AndreSantosCosta <andredscosta@gmail.com>
2016-11-16 12:31:59 +0000

    Inicial Commit

--- cardiologiapediatrica/Funcionario.java
+++ cardiologiapediatrica/Funcionario.java
@@ -6,14 +6,14 @@ import java.util.Calendar;
  * Created by andrecosta on 12/11/16.
  */
 public class Funcionario {
-    private int numeroIdentificacaoFiscal;
+    private int nif;
     private String nome;
     private Calendar dataNascimento;
     private Servico servico;
     private String morada;
     private int telefone;
     private String email;
-    private String habilitacao;
+    private String habilitacoes;
     private String funcaoDesempenhada;
 
 }
